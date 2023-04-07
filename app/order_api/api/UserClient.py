a1f4ddc6457bda5ffd09d581a3d183a523fa56e0
Ramnivas Dhaker <ramnivasdhaker@Ramnivass-MacBook-Pro-2.local>
2023-04-07 21:42:44 +0530

    setup order service

---
+++ app/order_api/api/UserClient.py
@@ -0,0 +1,19 @@
+from flask import session
+import requests
+
+host_url = 'http://0.0.0.0:5556'
+
+class UserClient:
+
+    @staticmethod
+    def get_user(api_key):
+        headers = {
+            'Authorization': api_key
+        }
+
+        response = requests.request(method="GET", url=f'{host_url}/api/user', headers=headers)
+        if response.status_code == 401:
+            return False
+
+        user = response.json()
+        return user
